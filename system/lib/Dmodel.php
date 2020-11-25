cfa24823a4e6c5e7a1e36e24bd855a4decdb6296
Emmanuel <ogahemmanuel75@gmail.com>
2020-11-25 22:08:51 +0100

    Update Dmodel.php

--- system/lib/Dmodel.php
+++ system/lib/Dmodel.php
@@ -4,9 +4,9 @@ class Dmodel{
     protected $db = array();
 
     public function __construct(){
-        $dsn = 'mysql:dbname=phpdb; host=localhost';
-        $user = 'root';
-        $pass = '';
+        $dsn = 'mysql:dbname=heroku_bdb11af8fb71ac4; host=us-cdbr-east-02.cleardb.com';
+        $user = 'bfedf18c81d695';
+        $pass = 'e98d41bf';
         $this->db = new Database($dsn, $user, $pass);
        
     }
